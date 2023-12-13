1aff3b451b35136ad9266c1b00f605e10e24a4d0
Tom Archer <tomarcher@Toms-MacBook-Pro-16.local>
2023-12-13 18:46:09 +1100

    init dotfiles

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      transparent = true,
+      style = "darker",
+      colors = {
+        fg = "#EDEDED",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
